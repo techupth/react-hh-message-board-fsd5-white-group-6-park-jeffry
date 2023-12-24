0be27c442a24c464b0bb19a5eb64ac849c4ccf1e
Astrick <mypiarenoo@gmail.com>
2023-12-24 20:00:33 +0700

    (Fixed): Input state in message board

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [todo, setTodo] = useState([]);
+  const addTodo = () => {
+    const newTodo = [...todo];
+    newTodo.push(inputMessage);
+    setTodo(newTodo);
+  };
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(event) => {
+              setInputMessage(event.target.value);
+            }}
+            value={inputMessage}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addTodo}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {todo.map((item, index) => {
+          return (
+            <div key={index} className="message">
+              <h1>{item}</h1>
+              <button className="delete-button">x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
